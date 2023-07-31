ad1db2040660f0864cc61abc856266bbeb57addf
Andrii Lavreniuk <lavreniuk.andrey@gmail.com>
2023-07-31 14:41:16 +0300

    added mock data for notes and App class

---
+++ src/javascript/app.js
@@ -0,0 +1,13 @@
+class App {
+    static root = document.getElementById('root');
+
+    static startApp() {
+        try {
+            //
+        } catch (error) {
+            //
+        }
+    }
+}
+
+export default App;
